cd7bf30cdd93d9a192c35ebb89829512b00ae05a
Pierce Moore <piercemoore@gmail.com>
2011-03-04 14:51:22 -0600

    Working version, moved around files to incorporate bg.html functionality. Lots of bg.html progress yet to be made though. 0.2 release is not too far away.

--- tabPageFiles/functions.js
+++ tabPageFiles/functions.js
@@ -18,81 +18,8 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
  * @author	: Pierce Moore
  */
 
-function showAddFb() {
-	chrome.tabs.getSelected(null, function(tab) {
-		// Build the HTML markup to be inserted into the facebox.
-		var html = "<h1>Edit Link Details</h1><h3>Title</h3><input type=\"text\" id=\"customLinkTitle\" value=\"" + tab.title + "\" /><h3>Full URL</h3><input type=\"text\" id=\"customLinkUrl\" value=\"" + tab.url + "\"  /><button id=\"customLinkFbSubmit\">Add</button>";
-		jQuery.facebox(html);
-	});
-}
-
-function doAddLink(title,url)
-{
-	//alert('Add new site to tab page working!');
-	/*
-	chrome.tabs.captureVisibleTab(null, function(dataUrl) {
-	content = document.getElementById('screenshots');
-	var image = document.createElement('img');
-	image.setAttribute('src', dataUrl);
-	content.appendChild(image);});
-	*/
-	if(addLink(title,url)) {
-		console.log(title + " with url " + url + " successfully added to new tab page.");
-		return true;
-	} else {
-		console.log(title + " with url " + url + " UNABLE to be added to new tab page.");
-		return false;
-	}
-}
-
-function displayLinks() {
-	if(debug) { console.log('Working on links now...'); }
-	var linkOutput = "";
-	var links = data.dataLinks.links;
-	if(debug) {
-		$.each(data.dataLinks.links,function(key,val) {
-			console.log("Key: " + key + ", Val: " + val);
-		});
-	}	
-	if(links) {
-		if(debug) { console.log("There are links in the database for parsing. Starting loop now."); }
-		for(var link in links) {
-			if(links.hasOwnProperty(link)) {
-				var currentLink = "<a style=\"background:url('linkImages/" + links[link].img + ") no-repeat top;\" href=\"" + links[link].href +"\"><h2>" + links[link].name + "</h2></a>";
-				linkOutput += currentLink;
-				console.log("Link " + link + ": " + currentLink);
-			}
-		}
-		$(linkOutput).appendTo("#linkStart");
-	} else {
-		console.log("No links were found. Something is screwy.");
-	}
-}
-
-function updateTime() {
-	var today = new Date();
-	var hour = today.getHours();
-	var min = today.getMinutes();
-	var sec = today.getSeconds();
-	if( hour < 10) {
-		hour = "0" + hour;
-	}
-	if( min < 10) {
-		min = "0" + min;
-	}
-	if(sec < 10) {
-		sec = "0" + sec;
-	}
-	document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
-	var day = today.toDateString();
-	document.getElementById("date").innerHTML = day;
-}
-
-// Google Analytics functions
-function trackButton() {
-    _gaq.push(['_trackEvent', 'Site added to new tab page using the "Add" button.']);
-};
-
-function trackAddBox() {
-    _gaq.push(['_trackEvent', 'Site added to new tab page using the text input box in the popup.']);
-};
+/*
+ *
+ * All of this functionality has been moved to the bg.html page, just like everything else. I didn't know it was this difficult to call functions on the background page, lol.
+ *
+ */
