1e07e70511650b89ac56bf28f9028eb9e5e01586
Pierce Moore <pierce@piercemoore.com>
2013-02-11 01:08:01 -0600

    Updated format of logging

--- js/modules/application.js
+++ js/modules/application.js
@@ -2,13 +2,13 @@
  * This is the Backbone Module that will be included in the new tab page
  */
 function log( msg ) {
-	console.log("BANTP Log", msg );
+	console.log("BANTP Log: ", msg );
 }
 function warn( msg ) {
-	console.warn("BANTP Warning", msg );
+	console.warn("BANTP Warning: ", msg );
 }
 function error( msg ) {
-	console.error("BANTP ERROR!!", msg );
+	console.error("BANTP ERROR: ", msg );
 }
 
 var loader = {
