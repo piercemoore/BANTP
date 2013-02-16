ba725ed988cb2a6b235a15951bf7b929f9d8a095
Pierce Moore <pierce@piercemoore.com>
2013-02-16 03:23:26 -0600

    Implementing all changes in index page

---
+++ js/templates.js
@@ -0,0 +1,53 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+
+  Handlebars.partials = Handlebars.templates;
+templates['appmanager_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div id=\"appmanager_workspace\">	<h1>APPLICATION MANAGEMENT</h1></div>";
+  });
+templates['bookmarks_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div id=\"bookmarks_workspace\">	<h1>BOOKMARK MANAGEMENT</h1></div>";
+  });
+templates['dashboard_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div id=\"dashboard_workspace\">	<h1>DASHBOARD</h1></div>";
+  });
+templates['modules_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div id=\"modules_workspace\">	<h1>MODULE MANAGEMENT</h1></div>";
+  });
+templates['settings_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div id=\"settings_workspace\">	<h1>BANTP SETTINGS</h1></div>";
+  });
+templates['todo_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div id=\"todo_workspace\">	<h1>TO-DO LIST APPLICATION</h1></div>";
+  });
+})();
