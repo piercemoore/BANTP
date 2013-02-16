6d489237ade712d5fdc03db6378c54ea8802c12b
Pierce Moore <pierce@piercemoore.com>
2013-02-16 05:27:02 -0600

    Bringing in BugSnag! :D

--- js/setup.js
+++ js/setup.js
@@ -1,1 +1,18 @@
 console.time("Page Build");
+
+Bugsnag.releaseStage = "development";
+Bugsnag.metaData = {
+	user : {
+		name : "Pierce Moore",
+		email : "pierce@piercemoore.com"
+	}
+};
+
+window.onerror = function windowError(msg, url, line) {
+	var pathParts = url.split("/");
+	pathParts.splice(0,3);
+	var path = pathParts.join("/");
+	Bugsnag.notify("Uncaught: '" + path + ", line #" + line + " :: " + msg );
+	error("Uncaught: '" + path + ", line #" + line + " :: " + msg );
+	return true;
+};
