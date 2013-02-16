593894022fd4187c3301ac0ddbb825203d174598
Pierce Moore <pierce@piercemoore.com>
2013-02-16 07:32:40 -0600

    Cleanup of background page

--- js/background.js
+++ js/background.js
@@ -5,3 +5,7 @@ chrome.tabs.onCreated.addListener(function(tab) {
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	console.log("Tab changed: #" + tabId, changeInfo, tab );
 });
+
+chrome.browserAction.onClicked.addListener(function() {
+	console.log("Browser action clicked!");
+});
