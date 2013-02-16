a8dc6903bbeb207b63fcde2f318501d27052ec07
Pierce Moore <pierce@piercemoore.com>
2013-02-16 09:20:56 -0600

    Set of generic functions to test the chrome.* APIs

--- js/background.js
+++ js/background.js
@@ -6,6 +6,26 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	console.log("Tab changed: #" + tabId, changeInfo, tab );
 });
 
-chrome.browserAction.onClicked.addListener(function() {
-	console.log("Browser action clicked!");
+chrome.storage.sync.get(null, function(items) {
+	console.log("All items in synced storage", items );
 });
+
+chrome.storage.sync.set({name : "Pierce Moore", _config : _config }, function() {
+	console.log("Set name to Pierce Moore");
+});
+
+chrome.storage.sync.get("name", function(items) {
+	console.log("Name in synced storage", items );
+});
+
+chrome.storage.sync.get(null, function(items) {
+	console.log("All items in synced storage", items );
+});
+
+chrome.storage.sync.getBytesInUse(null, function(bytes) {
+	console.log("Total bytes in use:" + bytes + " bytes");
+});
+
+var d = new Date();
+
+console.log(d.toUTCString());
