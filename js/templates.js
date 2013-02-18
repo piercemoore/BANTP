d7bc18e2dfafe20cfc6e156acad170fe8d8dfd71
Pierce Moore <pierce@piercemoore.com>
2013-02-18 04:34:54 -0600

    Adding quick launch handlebars template

--- js/templates.js
+++ js/templates.js
@@ -1,53 +1,76 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-
-  Handlebars.partials = Handlebars.templates;
-templates['appmanager_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+templates['appmanager_workspace'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div id=\"appmanager_workspace\">	<h1>APPLICATION MANAGEMENT</h1></div>";
+  return "<div id=\"appmanager_workspace\">\n	<h1>APPLICATION MANAGEMENT</h1>\n</div>";
   });
-templates['bookmarks_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+templates['bookmarks_workspace'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div id=\"bookmarks_workspace\">	<h1>BOOKMARK MANAGEMENT</h1></div>";
+  return "<div id=\"bookmarks_workspace\">\n	<h1>BOOKMARK MANAGEMENT</h1>\n</div>";
   });
-templates['dashboard_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+templates['dashboard_workspace'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div id=\"dashboard_workspace\">	<h1>DASHBOARD</h1></div>";
+  return "<div id=\"dashboard_workspace\">\n	<h1>DASHBOARD</h1>\n</div>";
   });
-templates['modules_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+templates['modules_workspace'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div id=\"modules_workspace\">	<h1>MODULE MANAGEMENT</h1></div>";
+  return "<div id=\"modules_workspace\">\n	<h1>MODULE MANAGEMENT</h1>\n</div>";
   });
-templates['settings_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+templates['quick_launch_single'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
-  
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  return "<div id=\"settings_workspace\">	<h1>BANTP SETTINGS</h1></div>";
+  buffer += "<a href=\"#\" data-app=\"";
+  if (stack1 = helpers.id) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.id; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" class=\"app-launch\">\n	<img src=\"";
+  if (stack1 = helpers.icon) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.icon; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" height=\"32\" width=\"32\" alt=\"";
+  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" title=\"";
+  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" />\n</a>";
+  return buffer;
   });
-templates['todo_workspace.handlebars'] = template(function (Handlebars,depth0,helpers,partials,data) {
+templates['settings_workspace'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div id=\"todo_workspace\">	<h1>TO-DO LIST APPLICATION</h1></div>";
+  return "<div id=\"settings_workspace\">\n	<h1>BANTP SETTINGS</h1>\n</div>";
+  });
+templates['todo_workspace'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div id=\"todo_workspace\">\n	<h1>TO-DO LIST APPLICATION</h1>\n</div>";
   });
 })();
