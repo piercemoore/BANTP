9b8f32a437bebd65a51eed90311810133ba69f68
Pierce Moore <pierce@piercemoore.com>
2013-02-18 15:48:57 -0600

    Now building out local application data on installation; Added methods

--- js/background.js
+++ js/background.js
@@ -9,22 +9,88 @@
 /**
  * First, let's do the super high level stuff: Install/Update events.
  */
-
-
-var processInstall = function() {
-	log("Processing install of BANTP");
-	// Store the configuration locally
-	chrome.storage.local.set(_config, function() {
-		ifSuccessful(function() {
-			log("Config stored locally");
+ 
+/**
+ *	Parse a list of all installed applications, extensions, and themes and locally store them in two objects to dramatically increase speed:
+ *		1) Quick launch applications (barebones, no themes or extensions)
+ *		2) Application management object (full list, with parsed icons)
+ *
+ *	@function parseInstalledApplications()
+ *	@author Pierce Moore
+ *	@date 2/18/13
+ */
+var parseInstalledApplications = function() {
+	chrome.management.getAll(function(apps) {
+		log(apps);
+		
+		var template = Handlebars.templates['quick_launch_single'];
+		$("#quick_launch .module").append( template( app ) );
+		
+		var quickLaunchApps = allApps = [];
+		
+		_.each(apps, function(app) {
+			var quick, appIcon, icon128found;
+			
+			// First we parse the app icon
+			if( !app.icons ) {
+				appIcon = chrome.extension.getURL("img/icon_128.png");		
+			} else {
+				_.each(app.icons, function(icon) {
+					if( icon128found )
+						break;
+					
+					if( icon.size == 128 && appIcon == null ) {
+						appIcon = icon.url;
+						icon128found = true;
+					} else if( icon.size == 48 && appIcon == null && icon128found == null ) {
+						appIcon = icon.url;
+					}
+				});
+				
+				// If we still have no joy, load the BANTP icon.
+				if( appIcon == null )
+						appIcon = chrome.extension.getURL("img/icon_128.png");
+			}
+			
+			quick.icon = appIcon;
+			app.icon = appIcon; // We are storing the full object for the app management area, so no need to duplicate the object
+			
+			// Well that was a huge pain in the ass. Let's move on.
+			
+			// let's do the quick launch bar.
+			if( app.enabled && app.isApp ) {
+				quick.id = app.id;
+				quick.url = app.appLaunchUrl;
+				quick.name = app.name;
+			}
+			
+			// Push them into our arrays to store
+			quickLaunchApps.push(quick);
+			allApps.push(app);
+		});
+		
+		// Now that's done, we store both objects locally
+		chrome.storage.local.set({ quickLaunchApps : quickLaunchApps, allApps : allApps }, function() {
+			if( chrome.runtime.lastError )
+				error( chrome.runtime.lastError );
+			else
+				log("Applications parsed and stored locally!");
 		});
 	});
+	
+};
 
-	// Create our context menu
+/**
+ *	Creates the context menu for BANTP upon installation
+ *
+ *	@function createContextMenu
+ *	@author Pierce Moore
+ *	@date 2/18/13
+ */
+var createContextMenu = function() {
 	config("contextMenu", function(properties) {
 		if( _.size(properties) ) {
 			chrome.contextMenus.create( properties, function() {
-				log("Evaluating success of context menu creation using properties: ", properties );
 				ifSuccessful(function() {
 					log("Context Menu created", properties );
 				}, properties, function(err, data) {
@@ -33,6 +99,48 @@ var processInstall = function() {
 			});	
 		}
 	});
+};
+
+var logStorage = function() {
+	chrome.storage.local.get(function(localData) {
+		log("All local BANTP data:", localData);
+	});
+	chrome.storage.sync.get(function(syncData) {
+		log("All synced BANTP data:", syncData);
+	});
+};
+
+/**
+ *	Pulls the default link set from config and stores them properly using the link management system
+ *
+ *	@function installDefaultLinks
+ *	@author Pierce Moore
+ *	@date 2/18/13
+ */
+var installDefaultLinks = function() {
+	config("links:default", function(links) {
+		_.each(links, function(link) {
+			//
+		});
+	});
+};
+
+var processInstall = function() {
+	log("Processing install of BANTP");
+	// Store the configuration locally
+	chrome.storage.local.set(_config, function() {
+		ifSuccessful(function() {
+			log("Config stored locally");
+			createContextMenu();
+			parseInstalledApplications();
+			installDefaultLinks();
+			
+		}, _config, function(err, data) {
+			error(err, data);
+		});
+	});
+	
+
 };
 
 var processUpdate = function() {
