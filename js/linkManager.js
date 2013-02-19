00b84b7224010b549e41820c5c713476bbbfd369
Pierce Moore <pierce@piercemoore.com>
2013-02-19 01:28:35 -0600

    Foundation of link management

---
+++ js/linkManager.js
@@ -0,0 +1,16 @@
+/**
+ *	This is the module that will handle all interaction with CRUD operations for links and link groups
+ *
+ *	@package BANTP
+ *	@file /Volumes/PIERCE-MBP/Users/pierce/GitHub/Personal/BANTP/js/linkManager.js
+ *	@author Pierce Moore
+ *	@date 2/18/13
+ **/
+ 
+var createLinkGroup = function(name, links) {
+	//
+};
+
+var addLink = function(groupId, link) {
+	//
+};
